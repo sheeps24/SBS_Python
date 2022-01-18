7b188d109fdb68411c0eff605571ea45db09c819
sheeps24 <zzztreez2@gmail.com>
2022-01-18 15:53:56 +0900

    4일차

---
+++ Day04/ex02.py
@@ -0,0 +1,13 @@
+
+
+name = 'PSW'
+print('my name is %s .' % name)
+
+height  = 188
+print('my height is %.2f cm .' % height)
+
+weight = 80
+print('my weight is %.2f kg .' % weight)
+
+year, month, day = 2022, 1, 18
+print('내 생일은 %d년 %d월 %d일 이다.' %(year, month, day))
